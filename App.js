b61a843cec2ed8c5df828f5ce0d950637b596376
GUSTAVO PIETRO DE ASSIS SILVA <gustavo.silva2148@etec.sp.gov.br>
2025-05-23 12:06:37 -0300

    versao 2

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,9 +12,11 @@ export default function App(){
   return(
     <NavigationContainer>
        <Stack.Navigator initialRouteName='Home'>
-         <Stack.Screen name="Home" component={HomeScreen}/>
+         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false}}/>
          <Stack.Screen name="Details" component={DetailsScreen}/>
          <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false}}/>
+         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false}}/> 
+       
        </Stack.Navigator>
     </NavigationContainer>
   );
